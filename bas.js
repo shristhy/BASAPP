2cbd517d1829497a1a330be40e8afe83d355478a
Xander Krause <sk8rak@gmail.com>
2015-02-05 14:09:42 -0800

    comitted the js validation, not currently working with bas.html

---
+++ bas.js
@@ -0,0 +1,43 @@
+
+
+    function validateForm()
+    {
+        var a=document.forms["mockupbas"]["fname"].value;
+        if (a == "" || a == null)
+        {
+            alert("You must enter a first name");
+            return false;
+        }
+        
+        var b=document.forms["mockupbas"]["lname"].value;
+        if (b == "" || b == null)
+        {
+            alert("You must enter a last name");
+            return false;
+        }
+        
+        
+        
+        var c=document.forms["mockupbas"]["sid"].value;
+        
+        if(newStudent == false) {
+            if(c.length != 9)
+            {
+                alert("This is not a valid SID");
+                return false;
+            }
+        }
+        
+        var d=document.forms["mockupbas"]["email"].value;
+        var atpos = d.indexOf("@");
+        var dospos = d.lastIndexOf(".");
+        if (d != "" || d != null)
+        {
+           if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 > d.length)
+            {
+                alert("This is not a valid email");
+                return false;
+            } 
+        }
+        return true;
+    }
